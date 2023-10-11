c8ec30f734a758bb2b77be3bf15082ecd2b4d69e
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-11 17:36:28 -0300

    Initial Commit

---
+++ ConvertMe/ConvertMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeApp.swift
+//  ConvertMe
+//
+//  Created by Adriano Valumin on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
