1f6bf2b15b81741163d6cb982a59b81cd290f170
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-14 21:13:50 -0300

    Finished app v1

--- ConvertMe/ContentView.swift
+++ ConvertMe/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State var temperature:Double?
+    @State var temperatureUnit = "Fahrenheit"
+    let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var celsiusToFarenheit:Double {
+        let celsius:Double = temperature ?? 0
+        let farenheit:Double = celsius * 1.8 + 32
+        return farenheit
     }
-}
+    
+    var celsiusToKelvin:Double {
+        let celsius:Double = temperature ?? 0
+        let kelvin:Double = celsius + 273.15
+        return kelvin
+    }
+    
+    var convertedTemperature: Double {
+        if temperature != nil {
+            if temperatureUnit == "Fahrenheit" {
+                return celsiusToFarenheit
+            } else if temperatureUnit == "Kelvin" {
+                return celsiusToKelvin
+            } else {
+                return temperature ?? 0
+            }
+        }
+        return 0
+    }
+
+        var body: some View {
+            NavigationStack {
+                Form {
+                    Section("Temperature Conversion") {
+                        TextField("Input temperature in Celsius", value: $temperature, format: .number)
+                        Picker("Unit", selection: $temperatureUnit) {
+                            ForEach(temperatureUnits, id: \.self) {
+                                Text($0)
+                            }
+                        }.pickerStyle(.segmented)
+                        Text("Converted Value: \(convertedTemperature.formatted())º \(temperatureUnit)")
+                    }
+                }.navigationTitle("ConvertMe")
+            }
+        }
+    }
 
 #Preview {
     ContentView()
